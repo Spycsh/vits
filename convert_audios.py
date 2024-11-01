ef2714a62189763aafda051441c9fa889b10956d
Spycsh <sihan.chen@intel.com>
2024-11-01 07:23:38 +0000

    remove useless

--- convert_audios.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-for i in os.listdir("../data/wei"):
-    # convert to wav, 1 channels, sr 22050
-    name = i.split(".")[0]
-    subprocess.run(f'ffmpeg -i ../data/wei/{i} -ac 1 -ar 22050 data/wei/{name}.wav -y', shell=True)
